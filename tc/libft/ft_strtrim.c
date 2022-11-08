2feb38534ae211986c94f42ddc675a8df630fa87
Ronen SHAY <rshay@e2r7p2.clusters.42paris.fr>
2022-11-08 13:54:44 +0100

    debug

--- tc/libft/ft_strtrim.c
+++ tc/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 18:54:51 by rshay             #+#    #+#             */
-/*   Updated: 2022/11/08 13:33:41 by rshay            ###   ########.fr       */
+/*   Updated: 2022/11/08 13:53:16 by rshay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	j = ft_strlen((char *)s1) - 1;
 	while (ft_instr(s1[j], set) && j)
 		j--;
+	if (i > j)
+		return 0;
 	s2 = (char *)malloc((j - i + 1) * sizeof(char));
+	if (!s2)
+		return (NULL);
 	k = 0;
 	while (i <= j)
 	{
