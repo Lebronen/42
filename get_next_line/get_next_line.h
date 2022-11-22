69a554b81152463d0fffaec45ab2f030e89f23c6
Lebronen <96152449+Lebronen@users.noreply.github.com>
2022-11-22 18:32:27 +0100

    listes chaînés

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,32 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rshay <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/16 14:11:00 by rshay             #+#    #+#             */
-/*   Updated: 2022/11/16 19:15:42 by rshay            ###   ########.fr       */
+/*   Created: 2022/11/22 18:26:08 by rshay             #+#    #+#             */
+/*   Updated: 2022/11/22 18:27:38 by rshay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 5
-#endif
+
 # include <stdlib.h>
+
+# include <sys/types.h>
+# include <sys/uio.h>
 # include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 
 typedef struct s_list
 {
-        void                    *content;
-        struct s_list   *next;
-}                               t_list;
+	char			*content;
+	struct s_list	*next;
+}				t_list;
 
-char    *get_next_line(int fd);
-int             ft_line_size(char *s);
-char    *ft_strncat(char *dest, char *src, int n);
-char    *ft_clear(char *str);
-int             in_str(char *str, char c);
-char    *ft_strncpy(char *dst, char *src, int n);
-
+char	*get_next_line(int fd);
+int		found_newline(t_list *stash);
+t_list	*ft_lst_get_last(t_list *stash);
+void	read_and_stash(int fd, t_list **stash);
+void	add_to_stash(t_list **stash, char *buf, int readed);
+void	extract_line(t_list *stash, char **line);
+void	generate_line(char **line, t_list *stash);
+void	clean_stash(t_list **stash);
+int		len(const char *str);
+void	free_stash(t_list *stash);
 #endif
