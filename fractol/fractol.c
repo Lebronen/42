05a4e509f261aae937ac609f56cdf3586fcbc52a
Lebronen <96152449+Lebronen@users.noreply.github.com>
2022-12-17 17:26:00 +0100

    zoom

--- fractol/fractol.c
+++ fractol/fractol.c
@@ -10,70 +10,85 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx-linux/mlx.h"
-#include "fractol.h"
-#include <stdio.h>
-
-void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-    char *dst;
-
-    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
-
-int main()
-{
-    
-    void *mlx;
-    void *mlx_win;
-    t_data img;
-    int i;
-    int j;
-
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 720, 460, "window");
-    img.img = mlx_new_image(mlx, 720, 460);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-    
-    
-    i = 0;
-    
-    while (i < 720)
-    {
-        j = 0;
-        while (j < 460)
-        {   
-            my_mlx_pixel_put(&img, i , j,  0x00FF0000);
-            j++;
-        }
-        i++;
-    }
-    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-    
-	mlx_loop(mlx);
-    /*
-   t_comp z;
-   int x;
-   z = coord_to_comp(720, 460, 460, 720);
-   z = prod_comp(z, z);
-   x = norme_comp(z);
-   printf("%f + i%f et |z| = %d\n", z.r, z.i, x);
-   */
-}
-
-t_comp coord_to_comp(int x, int y, int h, int w)
-{
-    t_comp pixel;
-    pixel.r = (2.0 * (double)x / w - 1.0);
-    pixel.i = (1.0 - 2.0 * (double)y / h);
-    return pixel;
-}
-
-t_comp  mandelbrot(t_comp c, int max)
-{
-    if (norme_comp(c) == 0 || norme_comp(c) >= max)
-        return (c);
-    return add_comp(mandelbrot(prod_comp(c, c), max), mandelbrot(c, max));
-    
-}
+#include "include/mlx.h"
+ 14 #include "fractol.h"
+ 15 
+ 16 void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
+ 17 {
+ 18     char    *dst;
+ 19 
+ 20     dst = data->addr + (y * data->line_l + x * (data->bpp / 8));
+ 21     *(unsigned int *)dst = color;
+ 22 }
+ 23 
+ 24 int main(void)
+ 25 {
+ 26     t_vars      vars;
+ 27     t_data      img;
+ 28 
+ 29     vars.mlx = mlx_init();
+ 30     vars.win = mlx_new_window(vars.mlx, W, H, "window");
+ 31     img.img = mlx_new_image(vars.mlx, W, H);
+ 32     img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_l, &img.endian);
+ 33     vars.img = &img;
+ 34     vars.color = 0;
+ 35     vars.zoom = 1;
+ 36     vars.x_min = -0.25;
+ 37     vars.x_max = 0.25;
+ 38     vars.y_max = 0.25;
+ 39     vars.y_min = -0.25;
+ 40     put_fractale(&img, 75, vars);
+ 41     mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+ 42     mlx_hook(vars.win, 17, 1L << 0, close, &vars);
+ 43     mlx_hook(vars.win, 2, 1L << 0, clavier, &vars);
+ 44     //mlx_mouse_hook(vars.win, dive, &vars);
+ 45     mlx_loop(vars.mlx);
+ 46 }
+ 47 
+ 48 void    put_fractale(t_data *img, int max, t_vars vars)
+ 49 {
+ 50     int     x;
+ 51     int     y;
+ 52     t_comp  c;
+ 53     int     i;
+ 54 
+ 55     x = 0;
+ 56     while (x < W)
+ 57     {
+ 58         y = 0;
+ 59         while (y < H)
+ 60         {
+ 61             c = coord_to_comp(x, y, vars);
+ 62             i = mandelbrot(c, max);
+ 63             if (i != max)
+ 64                 my_mlx_pixel_put(img, x, y, (i * 0xAB42EFFF) % 0xFFFFFFFF);
+ 65             y++;
+ 66         }
+ 67         x++;
+ 68     }
+ 69 }
+ 70 
+ 71 t_comp  coord_to_comp(int x, int y, t_vars vars)
+ 72 {
+ 73     t_comp  pixel;
+	 
+	pixel.r = vars.x_min + (vars.x_max - vars.x_min) * x / (W - 1);
+ 76     pixel.i = vars.y_min + (vars.y_max - vars.y_min) * y / (H - 1);
+ 77     return (pixel);
+ 78 }
+ 79 
+ 80 int mandelbrot(t_comp c, int max)
+ 81 {
+ 82     t_comp  z;
+ 83     int     i;
+ 84 
+ 85     i = 0;
+ 86     z.r = 0;
+ 87     z.i = 0;
+ 88     while (i < max && norme_comp(z) < 4)
+ 89     {
+ 90         z = add_comp(prod_comp(z, z), c);
+ 91         i++;
+ 92     }
+ 93     return (i);
+ 94 
