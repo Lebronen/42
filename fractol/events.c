30a2a7f689755300a1d4e6396c3f7b37454ea1ed
Lebronen <96152449+Lebronen@users.noreply.github.com>
2023-01-10 22:39:21 +0100

    Update events.c

--- fractol/events.c
+++ fractol/events.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   events.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lebronen <lebronen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 19:12:12 by rshay             #+#    #+#             */
-/*   Updated: 2023/01/10 19:26:47 by rshay            ###   ########.fr       */
+/*   Updated: 2023/01/10 22:39:03 by lebronen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-#include "include/mlx.h"
+#include "mlx.h"
 #include <stdio.h>
 
 int	close(t_vars *vars)
@@ -28,30 +28,37 @@ int	clavier(int keycode, t_vars *vars)
 		mlx_destroy_window(vars->mlx, vars->win);
 		mlx_destroy_display(vars->mlx);
 	}
+    printf("%d\n", keycode);
+    
 	
 	return (0);
 }
 
 int	dive(int button, int x, int y, t_vars *vars)
 {
-	t_comp p = coord_to_comp(x, y, *vars);
+    
 	if (button == 1)
 	{
-		vars->x_min = vars->x_min * 1.03;
-		vars->x_max = vars->x_max * 1.03;
-		vars->y_min = vars->y_min * 1.03;
-		vars->y_max = vars->y_max * 1.03;
-		vars->move_x = p.r;
-		vars->move_y = p.i;
+        double pre_long = vars->x_max - vars->x_min;
+        double longueur = pre_long / 2;
+        double pre_large = vars->y_max - vars->y_max;
+        double largeur = pre_large / 2;
+        vars->x_min = x - longueur / 2;
+        vars->x_max = x + longueur / 2;
+        vars->y_min = y  - largeur / 2;
+        vars->y_max = y + largeur / 2;
+            
 	}
 	else if (button == 3)
 	{
-		vars->x_min = vars->x_min / 1.03;
-		vars->x_max = vars->x_max / 1.03;
-		vars->y_min = vars->y_min / 1.03;
-		vars->y_max = vars->y_max / 1.03;
-		vars->move_x = p.r;
-		vars->move_y = p.i;
+		double pre_long = vars->x_max - vars->x_min;
+        double longueur = pre_long * 2;
+        double pre_large = vars->y_max - vars->y_max;
+        double largeur = pre_large * 2;
+        vars->x_min = x - longueur * 2;
+        vars->x_max = x + longueur * 2;
+        vars->y_min = y  - largeur * 2;
+        vars->y_max = y + largeur * 2;
 	}
 		put_fractale(vars->img, 75 , *vars);
 		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->img, 0, 0 );
