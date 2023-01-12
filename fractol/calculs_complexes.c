0926f71c1ad6d7180c7b7a87d5dbdc5ee4c30dc3
Lebronen <96152449+Lebronen@users.noreply.github.com>
2023-01-12 10:37:00 +0100

    Update calculs_complexes.c

--- fractol/calculs_complexes.c
+++ fractol/calculs_complexes.c
@@ -6,29 +6,32 @@
 /*   By: lebronen <lebronen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 17:50:41 by lebronen          #+#    #+#             */
-/*   Updated: 2022/12/11 14:50:29 by lebronen         ###   ########.fr       */
+/*   Updated: 2023/01/12 00:24:49 by rshay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include <math.h>
 
-t_comp  add_comp(t_comp a, t_comp b)
+t_comp	add_comp(t_comp a, t_comp b)
 {
-    t_comp res;
-    res.r = a.r + b.r;
-    res.i = a.i + b.i;
-    return res;
-}
-t_comp prod_comp(t_comp a, t_comp b)
-{
-    t_comp res;
-    res.r = (a.r * b.r) - (a.i * b.i);
-    res.i = (a.r * b.i) + (a.i * b.r);
-    return (res);
+	t_comp	res;
+
+	res.r = a.r + b.r;
+	res.i = a.i + b.i;
+	return (res);
 }
 
-int norme_comp(t_comp z)
+t_comp	prod_comp(t_comp a, t_comp b)
 {
-    return (z.r * z.r + z.i * z.i);
+	t_comp	res;
+
+	res.r = (a.r * b.r) - (a.i * b.i);
+	res.i = (a.r * b.i) + (a.i * b.r);
+	return (res);
+}
+
+int	norme_comp(t_comp z)
+{
+	return (z.r * z.r + z.i * z.i);
 }
