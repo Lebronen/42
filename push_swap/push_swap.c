ef29ded22969a9d9cb46d3a0bc9a185dc69b5bd8
Lebronen <96152449+Lebronen@users.noreply.github.com>
2023-02-10 18:17:32 +0100

    Update push_swap.c

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,87 +6,86 @@
 /*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 09:25:06 by lebronen          #+#    #+#             */
-/*   Updated: 2023/02/10 16:26:19 by rshay            ###   ########.fr       */
+/*   Updated: 2023/02/10 16:51:01 by rshay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_swap  stack;
-    
-    if (argc <= 1 || !bien_forme(argv, argc, &stack))
-    {
-        ft_printf("Error");
-        free(stack.a);
-        free(stack.b);
-        return (0);
-    }
-    
-    rotate_a(&stack);
+	t_swap	stack;
+
+	if (argc <= 1 || !bien_forme(argv, argc, &stack))
+	{
+		ft_printf("Error");
+		free(stack.a);
+		free(stack.b);
+		return (0);
+	}
+	rotate_a(&stack);
+	return (0);
 }
 
-int bien_forme(char ** a, int x, t_swap *stack)
+int	bien_forme(char **a, int x, t_swap *stack)
 {
-    int i;
+	int	i;
 
-    i = 1;
-    stack->a = malloc(sizeof(int) * x);
-    stack->b = malloc(sizeof(int) * x);
-    stack->len_a = x - 1;
-    stack->len_b = 0;
-    while (i < x)
-    {
-        if (!is_nbr(a[i]))
-            return (0);
-        if (in_tab(stack->a, ft_atoi(a[i])))
-            return (0);
-        (stack->a)[i - 1] = ft_atoi(a[i]);
-        i++;
-    }
-    return (1);
+	i = 1;
+	stack->a = malloc(sizeof(int) * x);
+	stack->b = malloc(sizeof(int) * x);
+	stack->len_a = x - 1;
+	stack->len_b = 0;
+	while (i < x)
+	{
+		if (!is_nbr(a[i]))
+			return (0);
+		if (in_tab(stack->a, ft_atoi(a[i])))
+			return (0);
+	(stack->a)[i - 1] = ft_atoi(a[i]);
+		i++;
+	}
+	return (1);
 }
 
-int in_tab(int *t, int x)
+int	in_tab(int *t, int x)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (t[i])
-    {
-        if (t[i] == x)
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (t[i])
+	{
+		if (t[i] == x)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int is_nbr(char *s)
+int	is_nbr(char *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] < 48 || s[i] > 57)
-            return (0);
-        i++;
-    }
-    return (1);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] < 48 || s[i] > 57)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-void    aff_tab(int *t)
+void	aff_tab(int *t)
 {
-    int i;
+	int	i;
 
-    i = 0;
-
-    while (t[i])
-    {
-        ft_printf("%d ", t[i]);
-        i++;
-    }
-    ft_printf("\n");
+	i = 0;
+	while (t[i])
+	{
+		ft_printf("%d ", t[i]);
+		i++;
+	}
+	ft_printf("\n");
 }
