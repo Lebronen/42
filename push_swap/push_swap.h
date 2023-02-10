cbe93f0b3f5ee6840086f73e7a1fc1599fd63807
Lebronen <96152449+Lebronen@users.noreply.github.com>
2023-02-10 18:17:54 +0100

    Update push_swap.h

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,40 +6,40 @@
 /*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 20:08:59 by lebronen          #+#    #+#             */
-/*   Updated: 2023/02/06 17:24:47 by rshay            ###   ########.fr       */
+/*   Updated: 2023/02/10 17:32:33 by rshay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
 typedef struct s_swap
 {
-    int *a;
-    int *b;
-    int len_a;
-    int len_b;
-}       t_swap;
+	int	*a;
+	int	*b;
+	int	len_a;
+	int	len_b;
+}		t_swap;
 
-int     bien_forme(char ** a, int x, t_swap *stack);
-int     in_tab(int *t, int x);
-int     is_nbr(char *s);
-void    aff_tab(int *t);
-void    swap_a(t_swap *stack);
-void    swap_b(t_swap *stack);
-void    swap_ab(t_swap *stack);
-void    push_a(t_swap *stack);
-void    push_b(t_swap *stack);
-void    rotate_a(t_swap *stack);
-void    rotate_b(t_swap *stack);
-void    reverse_rotate_a(t_swap *stack);
-void    reverse_rotate_b(t_swap *stack);
-void    rotate_ab(t_swap *stack);
-void    reverse_ab(t_swap *stack);
-int     verif(t_swap stack);
+int		bien_forme(char **a, int x, t_swap *stack);
+int		in_tab(int *t, int x);
+int		is_nbr(char *s);
+void	aff_tab(int *t);
+void	swap_a(t_swap *stack);
+void	swap_b(t_swap *stack);
+void	swap_ab(t_swap *stack);
+void	push_a(t_swap *stack);
+void	push_b(t_swap *stack);
+void	rotate_a(t_swap *stack);
+void	rotate_b(t_swap *stack);
+void	reverse_rotate_a(t_swap *stack);
+void	reverse_rotate_b(t_swap *stack);
+void	rotate_ab(t_swap *stack);
+void	reverse_ab(t_swap *stack);
+int		verif(t_swap stack);
 
 #endif
