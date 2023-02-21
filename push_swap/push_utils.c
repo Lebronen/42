b997e7bfe145b6fee83012897cb64d5d26f77a9c
Ronen SHAY <rshay@e1r7p17.clusters.42paris.fr>
2023-02-21 16:46:41 +0100

    algo

--- push_swap/push_utils.c
+++ push_swap/push_utils.c
@@ -6,7 +6,7 @@
 /*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 15:44:53 by rshay             #+#    #+#             */
-/*   Updated: 2023/02/21 15:26:56 by rshay            ###   ########.fr       */
+/*   Updated: 2023/02/21 16:39:08 by rshay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,22 @@ void	push_a(t_swap *stack)
 	temp = (stack->b)[0];
 	stack->len_b -= 1;
 	stack->len_a += 1;
-	na = malloc(sizeof(int) * stack->len_a);
-	nb = malloc(sizeof(int) * stack->len_b);
-	ft_memcpy(nb, stack->b + 1, (stack->len_b) * sizeof(int));
+	na = ft_calloc(stack->len_a + 1 ,sizeof(int));
+	nb = ft_calloc(stack->len_b + 1 ,sizeof(int));
+	if (stack->len_b)
+		ft_memcpy(nb, stack->b + 1, (stack->len_b) * sizeof(int));
 	na[0] = temp;
 	ft_memcpy(na + 1, stack->a, (stack->len_a) * sizeof(int));
-	stack->a = malloc(sizeof(int) * stack->len_a);
-	stack->b = malloc(sizeof(int) * stack->len_b);
-	ft_memcpy(stack->a, na, stack->len_a * sizeof(int));
-	ft_memcpy(stack->b, nb, stack->len_b * sizeof(int));
-	free(na);
-	free(nb);
+	free(stack->a);
+	free(stack->b);
+	stack->a = na;
+	stack->b = nb;
+	//stack->a = ft_calloc(stack->len_a, sizeof(int));
+	//stack->b = ft_calloc(stack->len_b, sizeof(int));
+	//ft_memcpy(stack->a, na, stack->len_a * sizeof(int));
+	//ft_memcpy(stack->b, nb, stack->len_b * sizeof(int));
+	//free(na);
+	//free(nb);
 }
 
 void	push_b(t_swap *stack)
@@ -50,15 +55,19 @@ void	push_b(t_swap *stack)
 	temp = (stack->a)[0];
 	stack->len_a -= 1;
 	stack->len_b += 1;
-	na = malloc(sizeof(int) * stack->len_a);
-	nb = malloc(sizeof(int) * stack->len_b);
-	ft_memcpy(na, stack->a + 1, (stack->len_a) * sizeof(int));
+	na = ft_calloc(stack->len_a + 1, sizeof(int));
+	nb = ft_calloc(stack->len_b + 1, sizeof(int));
+	if (stack->len_a)
+		ft_memcpy(na, stack->a + 1, (stack->len_a) * sizeof(int));
 	nb[0] = temp;
 	ft_memcpy(nb + 1, stack->b, (stack->len_b) * sizeof(int));
-	stack->a = malloc(sizeof(int) * stack->len_a);
-	stack->b = malloc(sizeof(int) * stack->len_b);
-	ft_memcpy(stack->a, na, stack->len_a * sizeof(int));
-	ft_memcpy(stack->b, nb, stack->len_b * sizeof(int));
-	free(na);
-	free(nb);
+	free(stack->a);
+	free(stack->b);
+	stack->a = na;
+	stack->b = nb;
+	//ft_memcpy(nb + 1, stack->b, (stack->len_b) * sizeof(int));
+	//stack->a = ft_calloc(stack->len_a, sizeof(int));
+	//stack->b = ft_calloc(stack->len_b, sizeof(int));
+	//ft_memcpy(stack->a, na, stack->len_a * sizeof(int));
+	//ft_memcpy(stack->b, nb, stack->len_b * sizeof(int));
 }
