d8c957ee79719c4ac2269f58d284455beed3481e
Ronen SHAY <rshay@e2r8p5.clusters.42paris.fr>
2023-05-23 15:58:27 +0200

    pipex

---
+++ pipex/include/pipex.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 15:10:30 by rshay             #+#    #+#             */
+/*   Updated: 2023/05/22 17:29:11 by rshay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+void	error(void);
+char	*find_path(char *cmd, char **envp);
+void	execute(char *argv, char **envp);
+void	child_process(char **argv, char **envp, int *fd);
+void	parent_process(char **argv, char **envp, int *fd);
+
+#endif
