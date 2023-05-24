86df7e350f3af1e48b51e8a25e052ba93d4c5078
Ronen SHAY <rshay@e3r10p7.clusters.42paris.fr>
2023-05-24 20:06:29 +0200

    absolute

---
+++ philosophers/include/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rshay <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 18:39:42 by rshay             #+#    #+#             */
+/*   Updated: 2023/05/24 18:47:53 by rshay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <pthread.h>
+
+#endif
