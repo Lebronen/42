2dc6fbcb3a5be6a9b7fcb0aea51782087a5f1d17
Lebronen78 <ronenshay78@gmail.com>
2024-09-03 10:54:34 +0200

    adding tests to cpp08/ex02

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rshay <rshay@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lebronen <lebronen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 15:20:27 by rshay             #+#    #+#             */
-/*   Updated: 2024/09/02 17:03:48 by rshay            ###   ########.fr       */
+/*   Updated: 2024/09/03 10:53:31 by lebronen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ int main() {
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
+	mstack.push(42);
+	mstack.push(19);
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -34,6 +35,16 @@ int main() {
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	it = mstack.begin();
+	while (it != ite)
+	{
+		(*it)++;
+		std::cout << *it << std::endl;
+		it++;
+	}
+	
+
 	
 	return 0;
 
